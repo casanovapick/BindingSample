a8177036d1032c2b4e614c25c860eec91bdea805
00357863 <wannasit.chaiphinan@krungsri.com>
2018-04-26 14:50:00 +0700

    add fabric

--- app/src/main/java/com/example/wannnasitchaiphinan/bindingsample/main/MainActivity.java
+++ app/src/main/java/com/example/wannnasitchaiphinan/bindingsample/main/MainActivity.java
@@ -5,15 +5,18 @@ import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.crashlytics.android.Crashlytics;
 import com.example.wannnasitchaiphinan.bindingsample.R;
 import com.example.wannnasitchaiphinan.bindingsample.card.CardActivity;
 import com.example.wannnasitchaiphinan.bindingsample.databinding.ActivityMainBinding;
+import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         binding.buttonViewCard.setOnClickListener(v -> startActivity(new Intent(this, CardActivity.class)));
     }
